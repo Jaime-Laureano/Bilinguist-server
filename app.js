2bb373fbf3e4c36d9e6e8b3fefe03eac8f475c15
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2022-04-08 10:43:03 +0100

    Working on Student and Teacher Routes

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const allRoutes = require("./routes/index.routes");
 app.use("/api", allRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/api", authRoutes);
-// const stuRoutes = require("./routes/student.routes");
-// app.use("/api", stuRoutes);
+const stuRoutes = require("./routes/student.routes");
+app.use("/api", stuRoutes);
 // const teaRoutes = require("./routes/teacher.routes");
 // app.use("/api", teaRoutes);
 // app.use(require("../routes/auth.routes"));
-// app.use(require("../routes/student.routes"));
-// app.use(require("../routes/teacher.routes"));
+// app.use(require("./routes/student.routes"));
+// app.use(require("./routes/teacher.routes"));
 
 require("./error-handling")(app);
 
--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,11 +1,25 @@
 const router = require("express").Router();
-const mongoose = require("mongoose");
-const axios = require("axios");
-const Student = require("../models/Student.model");
-const Teacher = require("../models/Teacher.model");
 const Message = require("../models/Message.model");
 const isLoggedIn = require("../middleware/isLoggedIn");
 const isStudent = require("../middleware/isStudent");
 
-router.use(isLoggedIn);
+// router.use(isLoggedIn);
 
+router.get("/student-profile", (req,res) => {
+    console.log("whats up")
+    res.json("student-profile")
+})
+
+router.get("/message-board", isStudent, (req,res) => {
+    res.json("message-board")
+})
+
+router.get("/video-chat", isStudent, (req,res) => {
+    res.json("video-chat")
+})
+
+router.get("/practice", isStudent, (req,res) => {
+    res.json("practice")
+})
+
+module.exports = router;
