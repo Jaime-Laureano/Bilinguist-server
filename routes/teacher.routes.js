2a2b7594274066fb82d094334b3fda1070921c33
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2022-04-11 11:13:16 +0100

    Completed teacher form model and routes

--- routes/teacher.routes.js
+++ routes/teacher.routes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const Message = require("../models/Message.model");
+const addTeacher = require("../models/AddTeacher.model")
 const isLoggedIn = require("../middleware/isLoggedIn");
 const isTeacher = require("../middleware/isTeacher");
+const AddTeacher = require("../models/AddTeacher.model");
 
 router.use(isLoggedIn);
 
@@ -21,9 +23,31 @@ router.get("/video-chat", isTeacher, (req,res) => {
     res.json("video-chat")
 })
 
-router.get("/post-ad", isTeacher, (req,res) => {
-    res.json("post-ad")
+// router.get("/add-teacher", isTeacher, (req,res) => {
+//     res.json("add-teacher")
+// })
+
+router.post("/add-teacher", isLoggedIn, async (req, res, next) => {
+    try {
+      const { fullName, languages, cityRec, langLevel, goals, funFact } = req.body;
+      const newTeach = new AddTeacher({ fullName, languages, cityRec, langLevel, goals, funFact });
+      await newTeach.save();
+      res.json({ message: "Succesfully created new listing"});
+    } catch (err) {
+      res.status(400).json({
+        errorMessage: "Something went wrong! " + err.message,
+      });
+    }
+  });
+
+
+
+
+router.get("/find-teacher", isTeacher, (req,res) => {
+    res.json("find-teacher")
 })
+
+
 
 router.post("/new-message", isTeacher, (req, res) => {
     
