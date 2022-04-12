f1ca4f60523f60d9434a40799eafbeb34edf70e8
JasminBower <jasmin.ibower@gmail.com>
2022-04-12 16:34:15 +0200

    post form fixed and allTeachers rendered on page

--- models/AddTeacher.model.js
+++ models/AddTeacher.model.js
@@ -1,17 +1,15 @@
 const { Schema, model } = require("mongoose");
 
-const addTeacherSchema = new Schema(
-  {
-    fullName: { type: String, required: true },
-    languages: { type: String, required: true },
-    cityRec: { type: String, required: true },
-    // price: { type: Number, required:true },
-    langLevel: { type: String, required: true },
-    goals: { type: String, required: true },
-    funFact: { type: String, required: true },
-
-  }
-);
+const addTeacherSchema = new Schema({
+	fullName: { type: String, required: true },
+	languages: { type: String, required: true },
+	cityRec: { type: String, required: true },
+	price: { type: Number, required: true },
+	langLevel: { type: String, required: true },
+	goals: { type: String, required: true },
+	funFact: { type: String, required: true },
+	city: { type: String, required: true },
+});
 
 const AddTeacher = model("AddTeacher", addTeacherSchema);
 
