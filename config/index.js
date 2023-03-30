c87822863ae724a29957ff8caa297e01fb877da5
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2023-03-30 17:09:51 +0100

    reconfiguring CORS for deployment

--- config/index.js
+++ config/index.js
@@ -32,11 +32,6 @@ module.exports = (app) => {
 
   // controls a very specific header to pass headers from the frontend
   // ! please configure the cors `origin` key so that you can accept the requests wherever they might be coming from
-  app.use(
-    cors({
-      origin: ["http://localhost:3000", process.env.ORIGIN],
-    })
-  );
 
   //test//
 
